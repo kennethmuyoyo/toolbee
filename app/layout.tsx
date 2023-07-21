d8190a529892c0877a04b90093742bb26c0f1267
Kenneth <kennethmuyoyo@gmail.com>
2023-07-21 21:40:31 +0300

    favicon3

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
+import MyHead from './head'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,6 +16,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <MyHead/>
       <body className={inter.className}>{children}</body>
     </html>
   )

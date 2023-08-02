46c57f60f18dfbbe5a36a8ebfb702b614a567811
Kenneth <kennethmuyoyo@gmail.com>
2023-08-02 18:43:29 +0300

    padding

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -18,7 +18,7 @@ const Navbar = () => {
           </a>
         </div>
         <div className="flex md:order-2">
-          <a href="#featuresection" className="z-40 font-poppins font-medium text-black bg-custom-yellow hover:bg-yellow-500 rounded-lg px-4 py-2 text-center md:text-[14px] text-[13px]">Featured Tools</a>
+          <a href="#featuresection" className="z-40 font-poppins font-medium text-black bg-custom-yellow hover:bg-yellow-500 rounded-lg px-4 py-3 text-center md:text-[14px] text-[13px]">Featured Tools</a>
           <button onClick={toggleOpen} type="button" className=" md:mr-16 inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200" aria-controls="navbar-sticky" aria-expanded="false">
             <span className="sr-only">Open main menu</span>
             <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
